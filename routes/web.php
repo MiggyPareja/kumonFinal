7d5a3ae39e181f236451a287f0d65168e4a27841
CARLOS MIGUEL PAREJA <PCN0587@dlsud.edu.ph>
2024-09-30 15:50:50 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,11 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TransactionsController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', [Dashboard::class, 'index'])->name('Dashboard.index');
-
+Route::get('/dashboard/pass', [Dashboard::class, 'pass'])->name('Dashboard.pass');
 
 Route::resource('Students', StudentController::class);
 Route::resource('Transactions', TransactionsController::class);
-Route::resource('Dashboard', Dashboard::class);
-
-Route::get('/dashboard/pass', [Dashboard::class, 'pass'])->name('Dashboard.pass');
 
 
 
